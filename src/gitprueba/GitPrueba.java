fcd21f7bd41f974204d3600cd40b0ed03b533bde
Alejandro <amolinavaldovinos@gmail.com>
2026-01-12 19:09:56 +0100

    He añadido una nueva clase y un comentario

--- src/gitprueba/GitPrueba.java
+++ src/gitprueba/GitPrueba.java
@@ -15,6 +15,7 @@ public class GitPrueba {
      */
     public static void main(String[] args) {
         System.out.println("Voy a empezar de 0");
+        System.out.println("Segunda prueba");
     }
     
 }
