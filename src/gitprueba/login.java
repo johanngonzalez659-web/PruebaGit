c9b388cbaf672e5c2d18c18734983640043d581b
Alejandro <amolinavaldovinos@gmail.com>
2026-01-13 17:58:39 +0100

    He creado una funcion

--- src/gitprueba/login.java
+++ src/gitprueba/login.java
@@ -1,4 +1,4 @@
-/*
+git swi/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -8,8 +8,8 @@ package gitprueba;
  *
  * @author IFC303_1
  */
-public class prueba2 {
-    public static void main(String[] args) {
-        System.out.println("A?ado una nueva clase");
+public class login {
+    void ValidarCorreo(){
+        System.out.println("Estoy empezado a crear el login....");
     }
 }
