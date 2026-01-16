0f3852b76233da3674a40935ef81a63846c2eaf9
Alejandro <amolinavaldovinos@gmail.com>
2026-01-16 17:50:38 +0100

    He terminado el login

---
+++ src/gitprueba/login.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gitprueba;
+
+/**
+ *
+ * @author IFC303_1
+ */
+public class login {
+    void ValidarCorreo(){
+        System.out.println("Estoy empezando a crear en login");
+        System.out.println("He terminado de crear el login");
+    }
+}  
